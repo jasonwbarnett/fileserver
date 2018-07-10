2068c0f7e89bfb853a8e57f8bf596b765f823e22
Jason Barnett <jason.w.barnett@gmail.com>
2018-07-10 08:49:43 -0700

    Remove duplicate http.File and http.FileSystem

--- net/http/fs.go
+++ net/http/fs.go
@@ -74,26 +74,7 @@ func mapDirOpenError(originalErr error, name string) error {
 	return originalErr
 }
 
-// A FileSystem implements access to a collection of named files.
-// The elements in a file path are separated by slash ('/', U+002F)
-// characters, regardless of host operating system convention.
-type FileSystem interface {
-	Open(name string) (File, error)
-}
-
-// A File is returned by a FileSystem's Open method and can be
-// served by the FileServer implementation.
-//
-// The methods should behave the same as those on an *os.File.
-type File interface {
-	io.Closer
-	io.Reader
-	io.Seeker
-	Readdir(count int) ([]os.FileInfo, error)
-	Stat() (os.FileInfo, error)
-}
-
-func dirList(w http.ResponseWriter, r *http.Request, f File) {
+func dirList(w http.ResponseWriter, r *http.Request, f http.File) {
 	dirs, err := f.Readdir(-1)
 	if err != nil {
 		logf(r, "http: error reading directory: %v", err)
@@ -125,7 +106,7 @@ type jsonDirItem struct {
 	Size    int64     `json:"size,omitempty"`
 }
 
-func jsonDirList(w http.ResponseWriter, r *http.Request, f File) {
+func jsonDirList(w http.ResponseWriter, r *http.Request, f http.File) {
 	dirs, err := f.Readdir(-1)
 	if err != nil {
 		logf(r, "http: error reading directory: %v", err)
